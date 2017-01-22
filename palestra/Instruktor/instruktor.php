0f139ed616cd96c5dfa6220d6890d51c1aaa0024
OrgesKreka <orgeskreka1995@gmail.com>
2017-01-22 19:00:58 +0100

    Revert "Puna deri 22 / 1 / 2017"
    This reverts commit fa0dbbfc20ee3039412f6d9c4840d58a7270ca5f.

--- palestra/Instruktor/instruktor.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include 'includes/header.php';
-    include 'includes/navigation.php';
-    include 'includes/dashboard.php';
-?>
--- palestra/admin/includes/dil.php
+++
@@ -1,6 +0,0 @@
-<?php  
-	session_start();
-	session_destroy();
-	header( 'Location: /palestra/index.php' );
-	exit();
-?>
