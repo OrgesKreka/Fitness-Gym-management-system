b0f96be9962ff02f98393560422766f0b4a18154
OrgesKreka <orgeskreka1995@gmail.com>
2017-01-22 19:07:45 +0100

    commit - 3

---
+++ PalestraX/admin/includes/dashboard.php
@@ -0,0 +1,53 @@
+ <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#" class="text-center"><i class="glyphicon glyphicon-home"></i> <span>Paneli Administratorit</span> <span class="sr-only">(current)</span></a></li>
+            <li><a href="#"><i class="glyphicon glyphicon-envelope"></i><span>&nbsp; Inbox( 0 )</span></a></li>
+            </ul>
+
+            <ul class="nav nav-sidebar">
+                <li><a href="#"><i class="glyphicon glyphicon-user"></i> <span>&nbsp; Anëtarët</span></a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-plus-sign"></i><span>&nbsp; Shto Anëtar</span></a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-minus-sign"></i><span>&nbsp; Hiq Anëtar</span></a></li>
+          </ul>
+
+          <ul class="nav nav-sidebar">
+            <li><a href="#"><i class="glyphicon glyphicon-usd"></i><span>&nbsp;Pagesat</span></a></li>
+            <li><a href=""><i class="glyphicon glyphicon-exclamation-sign"></i><span>&nbsp;Pagesat e Anetareve</span></a></li>
+            <li><a href=""><i class="glyphicon glyphicon-alert"></i><span>&nbsp;Anetaret pa paguar</span></a></li>
+            <li><a href=""><i class="glyphicon glyphicon-user"></i><span>&nbsp; Instruktoret</span></a></li>
+            <li><a href=""><i class="glyphicon glyphicon-calendar"></i><span>&nbsp; Kalendari Sot</span></a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href=""><i class="glyphicon glyphicon-file"></i><span>&nbsp; Kurset</span></a></li>
+          </ul>
+        </div>
+
+    
+    <!-- Permbajtja Dashboard -->
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header text-center">Dashboard</h1>
+
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Label</h4>
+              <span class="text-muted">Something else</span>
+            </div>
+          </div>
---
+++ PalestraX/includes/modal_regjistrimi.php
@@ -0,0 +1,40 @@
+<!-- Forma Modale e regjistrimit-->
+  <div class="modal fade" id="modalRegjistrimi" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header" style="padding:35px 50px;">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4><span class="glyphicon glyphicon-lock"></span> Regjistrohu</h4>
+        </div>
+        <div class="modal-body" style="padding:40px 50px;">
+          <form role="form">
+            <div class="form-group">
+              <label for="usrname"><span class="glyphicon glyphicon-user"></span>Email</label>
+              <input type="text" class="form-control" id="usrname" placeholder="jepni emailin">
+            </div>
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Fjalekalimi</label>
+              <input type="password" class="form-control" id="psw" placeholder="Jepni fjalekalimin">
+            </div>
+
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Konfirmo Fjalekalimin</label>
+              <input type="password" class="form-control" id="psw" placeholder="Jepni fjalekalimin">
+            </div>
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Kodi Regjistrimit</label>
+              <input type="text" class="form-control" id="psw" placeholder="Kodi regjistrimit">
+            </div>
+              <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Regjistrohu</button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Anulo</button>
+        </div>
+      </div>
+      
+    </div>
+  </div> 
+</div>
